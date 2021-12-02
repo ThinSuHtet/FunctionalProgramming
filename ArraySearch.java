90988a29a6a895b7b9710e5b50616a2864900ba9
ThinSuHtet <89969856+ThinSuHtet@users.noreply.github.com>
2021-12-02 14:49:45 +0000

    Add Search function for Array

--- ArraySearch.java
+++ ArraySearch.java
@@ -1,8 +1,20 @@
 class ArraySearch {
+  public static double searchMin(double speedArr[]){
+      int arrSize = speedArr.length;
+      double min = speedArr[0];
+      for(int i=1;i<arrSize;i++){
+        if(speedArr[i] < min){
+          min= speedArr[i];
+        }
+      }
+      return min;
+  }
   public static void main(String[] args) {
-    public double searchMin(double speedArr[]){
-      
+    double[] speedArr = {7.78,6.54,4.54,5.67,4.66,6.99,9.8,6.55};
+    double min= searchMin(speedArr);
+    //System.out.println("ArraySearch.main()");
+    System.out.println("Minimum value: "+ min);
 
-    }
+    
   }
 }
